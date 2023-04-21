bbb40c07b4ce0ce66e7ad465428e716870138016
BASIL SABU <basilsabu5556@gmail.com>
2023-04-21 23:53:22 +0530

    Update cargame.py

--- cargame.py
+++ cargame.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.locals import *
 
-size=width,height=(800,800)
+size=width,height=(1200,800)
 road_width = int (width/1.6)
 roadmark_width=int(width/80) 
 pygame.init()
@@ -22,9 +22,19 @@ pygame.draw.rect(
     screen,(255,240,60),(width/2+road_width/2- roadmark_width*3,0,roadmark_width,height)
 )
 pygame.display.update()
+car=pygame.image.load("car.png")
+car_loc=car.get_rect()
+car_loc.center=width/2 + road_width/4,height*0.8
+
+car2=pygame.image.load("othercar.png")
+car2_loc=car2.get_rect()
+ca2r_loc.center=width/2 - road_width/4,height+0.8
 while running:
     for event in pygame.event.get():
         if event.type ==QUIT:
             running=False
+            
+    screen.blit(car,car_loc)
+    pygame.display.update()
 
 pygame.quit()
